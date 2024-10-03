9c9ec4901b2d168708086f6617af298b73122b10
Дар`я Бількевич <ipz202_bdv@student.ztu.edu.ua>
2024-10-03 10:58:41 +0300

    Added log in ref on home screen, sign up ref on log in screen

--- frontend/screens/LoginScreen.js
+++ frontend/screens/LoginScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import Toast from 'react-native-toast-message';
 import { login } from '../services/authService';
@@ -52,6 +52,14 @@ const LoginScreen = () => {
         formData={formData}
         setFormData={setFormData}
       />
+      <View className="mt-4 flex-row justify-center items-center">
+        <Text className="text-purple-700">Don't have an account? </Text>
+        <TouchableOpacity onPress={() => navigation.navigate('Sign Up')}>
+          <Text className="text-purple-700 font-semibold underline">
+            Sign Up
+          </Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
