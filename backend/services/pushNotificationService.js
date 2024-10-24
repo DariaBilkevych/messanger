340e0ce732bf89d42c4ab539bde165e28d47d565
Дар`я Бількевич <ipz202_bdv@student.ztu.edu.ua>
2024-10-24 13:02:42 +0300

    Updated sending push notifications from server depending on the message type

--- backend/services/pushNotificationService.js
+++ backend/services/pushNotificationService.js
@@ -3,11 +3,27 @@ const expo = new Expo();
 
 export const sendPushNotification = async (receiver, populatedMessage) => {
   if (receiver.expoPushToken && Expo.isExpoPushToken(receiver.expoPushToken)) {
+    let messageBody;
+
+    switch (populatedMessage.messageType) {
+      case 'text':
+        messageBody = `${populatedMessage.senderId.firstName} ${populatedMessage.senderId.lastName}: ${populatedMessage.message}`;
+        break;
+      case 'image':
+        messageBody = `${populatedMessage.senderId.firstName} ${populatedMessage.senderId.lastName} sent an image.`;
+        break;
+      case 'file':
+        messageBody = `${populatedMessage.senderId.firstName} ${populatedMessage.senderId.lastName} sent a file.`;
+        break;
+      default:
+        messageBody = `${populatedMessage.senderId.firstName} ${populatedMessage.senderId.lastName} sent a message.`;
+    }
+
     const pushMessages = [
       {
         to: receiver.expoPushToken,
         sound: 'default',
-        body: `${populatedMessage.senderId.firstName} ${populatedMessage.senderId.lastName}: ${populatedMessage.message}`,
+        body: messageBody,
         data: { message: populatedMessage },
       },
     ];
