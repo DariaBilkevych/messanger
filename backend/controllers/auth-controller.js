e40958d1724be91efe5c23ece1cba010b38f9f7f
Дар`я Бількевич <ipz202_bdv@student.ztu.edu.ua>
2024-10-31 19:49:14 +0200

    Tried to fix problem with backend in build v.0.2

--- backend/controllers/auth-controller.js
+++ backend/controllers/auth-controller.js
@@ -94,11 +94,10 @@ export const login = async (req, res) => {
       refreshToken,
     });
   } catch (error) {
+    console.log(error);
     res
       .status(500)
       .json({ message: 'Something went wrong', error: error.message });
-
-    console.log(error.message);
   }
 };
 
