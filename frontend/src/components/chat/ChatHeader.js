c0fc5ffaa0eb34271e3aacd3165d36c607dca5cc
Дар`я Бількевич <ipz202_bdv@student.ztu.edu.ua>
2024-11-04 15:02:03 +0200

    Reduced large indentation in headers due to SafeArea

--- frontend/src/components/chat/ChatHeader.js
+++ frontend/src/components/chat/ChatHeader.js
@@ -1,5 +1,11 @@
 import React from 'react';
-import { View, Text, Image, TouchableOpacity } from 'react-native';
+import {
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  SafeAreaView,
+} from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { Ionicons } from '@expo/vector-icons';
 
@@ -7,20 +13,22 @@ const ChatHeader = ({ receiverName, receiverAvatar }) => {
   const navigation = useNavigation();
 
   return (
-    <View className="flex-row items-center p-3 border-b border-purple-200 mb-5 mt-10 justify-between">
-      <TouchableOpacity onPress={() => navigation.goBack()}>
-        <Ionicons name="chevron-back" size={24} color="black" />
-      </TouchableOpacity>
-      <View className="flex-row items-center">
-        <Text className="text-md font-semibold">{receiverName}</Text>
+    <SafeAreaView>
+      <View className="flex-row items-center p-2 border-b border-purple-200 mb-5 justify-between">
+        <TouchableOpacity onPress={() => navigation.goBack()}>
+          <Ionicons name="chevron-back" size={24} color="black" />
+        </TouchableOpacity>
+        <View className="flex-row items-center">
+          <Text className="text-md font-semibold">{receiverName}</Text>
+        </View>
+        <View className="flex-row items-center">
+          <Image
+            source={{ uri: receiverAvatar }}
+            className="w-9 h-9 rounded-full mr-2"
+          />
+        </View>
       </View>
-      <View className="flex-row items-center">
-        <Image
-          source={{ uri: receiverAvatar }}
-          className="w-9 h-9 rounded-full mr-2"
-        />
-      </View>
-    </View>
+    </SafeAreaView>
   );
 };
 
